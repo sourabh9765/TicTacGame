9ccede54dfb2ab73521680b65dc9ffcdde08796a
sourabh9765 <sourabh123459@gmail.com>
2017-03-30 18:07:54 +0530

    Initial commit

---
+++ app/src/main/java/com/example/sourabh/tictacgame/SplashScreen.java
@@ -0,0 +1,41 @@
+package com.example.sourabh.tictacgame;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by sourabh on 30/03/2017.
+ */
+
+public class SplashScreen extends AppCompatActivity {
+    // Splash screen timer
+    private static int SPLASH_TIME_OUT = 5000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash);
+
+        new Handler().postDelayed(new Runnable() {
+
+            /*
+             * Showing splash screen with a timer. This will be useful when you
+             * want to show case your app logo / company
+             */
+
+            @Override
+            public void run() {
+                // This method will be executed once the timer is over
+                // Start your app main activity
+                Intent i = new Intent(SplashScreen.this, Board.class);
+                startActivity(i);
+
+                // close this activity
+                finish();
+            }
+        }, SPLASH_TIME_OUT);
+    }
+}
+
